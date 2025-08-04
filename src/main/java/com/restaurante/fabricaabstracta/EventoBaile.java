377ed96604ff3894c7c54774aaf61a8f6c032eba
aldair <aldair@DESKTOP-T08N5JQ>
2025-08-04 12:10:44 -0700

    cherry picking the fabrica abstracta draft implementation into this branch

---
+++ src/main/java/com/restaurante/fabricaabstracta/EventoBaile.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.restaurante.fabricaabstracta;
+
+import com.restaurante.Evento;
+
+/**
+ *
+ * @author aldair
+ */
+public class EventoBaile extends Evento {
+
+    public EventoBaile() {
+// aqui se settean las propiedades del evento, tipo baile, idTipoEvento, y creo ya
+//        tipo = "baile"; 
+//        idTipoEvento = 1;
+    }
+
+}
